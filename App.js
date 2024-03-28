88fcf83a79cbb785d94691c4ed5aacc2da754254
AlinA19994 <123962281+AlinA19994@users.noreply.github.com>
2024-03-28 11:44:25 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Header } from "./Components/Header/header";
+import { Products } from "./Components/Products/Products";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Products dfg />
+    </div>
+  );
+}
+
+export default App;
